4b56b4869844ad78f873cb3336d8a41a3f9d4c1b
kuru <Knium@1st-Knium.local>
2017-03-01 03:33:16 +0900

    websocket chat tester

---
+++ public/client.js
@@ -0,0 +1,25 @@
+(function() {
+  var HOST = 'ws://localhost:3000/api/chatlog';
+  var ws = new WebSocket(HOST);
+
+  var form = document.querySelector('.form');
+
+  form.onsubmit = function() {
+    var input = document.querySelector('.input');
+    var text = input.value;
+    ws.send(text);
+    input.value = '';
+    input.focus();
+    return false;
+  }
+
+  ws.onmessage = function(msg) {
+    var response = msg.data;
+    var messageList = document.querySelector('.messages');
+    var li = document.createElement('li');
+    li.textContent = response;
+    messageList.appendChild(li);
+    console.log(msg);
+  }
+}());
+
