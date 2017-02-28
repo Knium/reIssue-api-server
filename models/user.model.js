03d37dd92183bd241cd00af75e4373f42752dfde
Passawee Jantakorn <passawee.jan@gmail.com>
2017-02-28 15:14:39 +0900

    add static method list

--- models/user.model.js
+++ models/user.model.js
@@ -7,4 +7,14 @@ const userSchema = new mongoose.Schema({
     created: { type: Date, default: Date.now }
 });
 
+userSchema.statics = {
+    list({ skip = 0, limit = 50 } = {}) {
+        return this.find()
+        .sort({ created: -1 })
+        .skip(skip)
+        .limit(limit)
+        .exec();
+    }
+};
+
 module.exports = mongoose.model("User", userSchema);
