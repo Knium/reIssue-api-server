32cf755b9d2db635ca10bf97de1a78ab23066dac
Passawee Jantakorn <passawee.jan@gmail.com>
2017-03-01 23:38:00 +0900

    review create

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -6,4 +6,20 @@ function get(req, res) {
       (reviews) => { res.json(reviews); });
 }
 
-module.exports = { get };
+function create(req, res) {
+  const review = new Review({
+    subject: req.subject,
+    created: Date.now,
+    reviewer: req.reviewer, // ちなみに君誰を格納するユーザのID
+    title: req.title,
+    text: req.text,
+    star: req.star, // オススメ度的な,
+    teacher: req.String,
+    result: req.result, // そんな偉そうなレビュー書いてるけど君秀とったの？ 0 -> 不可, 1 -> 可, .. 4 -> 秀 という風な感じかな
+  });
+  review.save()
+  .then(
+    (savedReview) => { res.json(savedReview); });
+}
+
+module.exports = { get, create };
--- routes/review.route.js
+++ routes/review.route.js
@@ -4,6 +4,7 @@ const reviewCtrl = require('../controllers/review.controller');
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.route('/')
-    .get(reviewCtrl.get);
+    .get(reviewCtrl.get)
+    .post(reviewCtrl.create);
 
 module.exports = router;
