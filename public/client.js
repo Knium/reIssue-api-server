521921009c83d32d4c0923a48cfc6219f568de63
Passawee Jantakorn <passawee.jan@gmail.com>
2017-03-01 13:47:31 +0900

    dividing chatroom

--- public/client.js
+++ public/client.js
@@ -1,16 +1,16 @@
 (function() {
   var HOST = 'ws://localhost:3000/api/chatlog';
   var ws = new WebSocket(HOST);
-  const room = { 0: 'A', 1: 'B' };
+  const room = { 0: '58b552de14d5f1260bed8574', 1: '58b5550314d5f1260bed8575' };
   const rand = Math.floor( Math.random() * 2);
-  console.log(room[rand]);
   var form = document.querySelector('.form');
+  console.log(room[rand]);
   ws.onopen = function () { // 接続を確認してからルーム別用の識別子msgを送る．
-     ws.send('reIssueWSConnect://58b5550314d5f1260bed8575');
+     ws.send(`reIssueWSConnect://${room[rand]}`);
   };
   form.onsubmit = function() {
     var input = document.querySelector('.input');
-    var text = `reIssueWSChat://58b5550314d5f1260bed8575/?text=${input.value}&speaker=111111111111111111111111`;
+    var text = `reIssueWSChat://${room[rand]}/?text=${input.value}&speaker=111111111111111111111111`;
     ws.send(text);
     input.value = '';
     input.focus();
