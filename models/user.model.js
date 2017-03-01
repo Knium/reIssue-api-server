f17947601a5652d442f17d411ef673a62d8b3424
Passawee Jantakorn <passawee.jan@gmail.com>
2017-03-01 17:13:20 +0900

    chg: timeschedule

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,14 @@ const mongoose = require('../config');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const userSchema = new mongoose.Schema({
   username: String,
+  taking: { // 月曜1限の履修している科目を見るときは user.taking.mon[0] みたいな, 履修していないところはnullにする．
+    Mon: [],
+    Tue: [],
+    Wed: [],
+    Thu: [],
+    Fri: [],
+    Sat: [],
+  },
   completed: [ObjectId],  // 取った科目
   failedClass: [ObjectId], // 落とした科目
   created: { type: Date, default: Date.now },
