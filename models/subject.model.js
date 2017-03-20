f409924e74989da6bc581a6fbf93dbdb9b1271ca
Passawee Jantakorn <passawee.jan@gmail.com>
2017-03-20 13:42:27 +0900

    科目別過去レポ機能

--- models/subject.model.js
+++ models/subject.model.js
@@ -5,7 +5,7 @@ const subjectSchema = new mongoose.Schema({
   name: String,
   teacher: String,
   taking: [ObjectId], // 誰が履修をしているか．各ユーザのObjectIDを要素にした配列
-  pastReport: [String], // 過去レポのURL
+  pastReport: [ObjectId], // 過去レポのURL
   classRoom: String, // 教室名
   semester: Number, // 第何学期から履修できるのか 自然数 1年前期は1，1年後期は2, ... 4年後期は8といった風に
   dayOfTheWeek: String, // Mon, Tues ... Sat
