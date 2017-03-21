908f3ea57ec7e2d766a2d204acc65855e46232a4
Passawee Jantakorn <passawee.jan@gmail.com>
2017-03-21 17:01:29 +0900

    a

--- controllers/subject.controller.js
+++ controllers/subject.controller.js
@@ -10,4 +10,40 @@ function _get() {
   return Subject.find({}).exec();
 }
 
-module.exports = { get, _get };
+function getByCourse(req, res) {
+  console.log(req.body);
+  Subject.find({}).exec()
+    .then(
+      (subjects) => {
+        const returnJSON = {
+          Mon: [null, null, null, null, null, null, null],
+          Tue: [null, null, null, null, null, null, null],
+          Wed: [null, null, null, null, null, null, null],
+          Thu: [null, null, null, null, null, null, null],
+          Fri: [null, null, null, null, null, null, null],
+        };
+        subjects.map((subject) => {
+          subject.course.map((type) => {
+            if (req.body.course === type) returnJSON[subject.dayOfWeek][subject.period - 1] = { name: subject.name, _id: subject._id };
+            });
+        });
+        res.json(returnJSON);
+      });
+}
+
+function getByDayAndPeriod(req, res) {
+  const period = parseInt(req.query.period, 10);
+  const dayOfWeek = req.query.dayOfWeek;
+  Subject.find({ period, dayOfWeek }, { course: 0, period: 0, dayOfWeek: 0 }).exec()
+  .then(
+    (subjects) => {
+      res.json(subjects);
+    });
+}
+
+module.exports = {
+  get,
+  _get,
+  getByCourse,
+  getByDayAndPeriod,
+};
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -14,26 +14,18 @@ function get(req, res) {
  * Create new user
  * @returns {User}
  */
-function create(req, res, next) {
+function create(req, res) {
     // todo: Twitter認証を超えたら登録できるようにする．
   const user = new User({
-    username: req.username,
-    taking: {
-      Mon: [null, null, null, null, null, null, null],
-      Tue: [null, null, null, null, null, null, null],
-      Wed: [null, null, null, null, null, null, null],
-      Thu: [null, null, null, null, null, null, null],
-      Fri: [null, null, null, null, null, null, null],
-      Sat: [null, null, null, null, null, null, null],
-    },
+    username: req.body.username,
+    taking: {},
     completed: [],
     failedClass: [],
     created: Date.now(),
   });
 
   user.save()
-  .then(savedUser => res.json(savedUser))
-  .catch(e => next(e));
+  .then((savedUser) => { res.json(savedUser); });
 }
 
 function getUserInfo(req, res) {
